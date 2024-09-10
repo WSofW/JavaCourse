9e17f372aa3152c170adcfdb7d58d795657e3c13
Sofa <sofiamaslo2005@gmail.com>
2024-09-10 18:26:21 +0300

    second tasks

--- first_lesson/src/Main.java
+++ first_lesson/src/Main.java
@@ -26,4 +26,52 @@ public class Main {
             System.out.println("5 * " + i + " = " + (5 * i));
         }
     }
+
+    // Задача 4
+    private static void sumOfNumbers(Scanner scanner) {
+        System.out.print("Введите целое число N: ");
+        int N = scanner.nextInt();
+        int sum = (N * (N + 1)) / 2;
+        System.out.println("Сумма от 1 до " + N + ": " + sum);
+    }
+
+    // Задача 5
+    private static void fibonacciNumbers(Scanner scanner) {
+        System.out.print("Введите целое число N для Фибоначчи: ");
+        int N = scanner.nextInt();
+        int aFib = 0, bFib = 1;
+        System.out.print("Первые " + N + " чисел Фибоначчи: ");
+        for (int i = 0; i < N; i++) {
+            System.out.print(aFib + " ");
+            int nextFib = aFib + bFib;
+            aFib = bFib;
+            bFib = nextFib;
+        }
+        System.out.println();
+    }
+
+    // Задача 6
+    private static void checkPrimeNumber(Scanner scanner) {
+        System.out.print("Введите целое число: ");
+        int num = scanner.nextInt();
+        boolean isPrime = num > 1;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(num + (isPrime ? " является простым числом." : " не является простым числом."));
+    }
+
+    // Задача 7
+    private static void printNumbersInReverse(Scanner scanner) {
+        System.out.print("Введите целое число N для обратного порядка: ");
+        int N = scanner.nextInt();
+        System.out.print("Числа от " + N + " до 1: ");
+        for (int i = N; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
